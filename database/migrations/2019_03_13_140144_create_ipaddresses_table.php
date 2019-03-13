857edefcdfb420588e0d69ca22bd0437e8a5766b
Stephen Fu <stephen.frx@gmail.com>
2019-03-13 15:56:23 +0800

    update ip address table

--- database/migrations/2019_03_13_140144_create_ipaddresses_table.php
+++ database/migrations/2019_03_13_140144_create_ipaddresses_table.php
@@ -17,10 +17,10 @@ class CreateIpaddressesTable extends Migration
             $table->increments('id');
             $table->string('floor', 20)->nullable()->comment('楼层');
             $table->string('room', 20)->nullable()->comment('房间号');
-            $table->int('ip')->default(0)->comment('IP地址');
+            $table->unsignedInteger('ip', 10)->default(0)->comment('IP地址');
             $table->string('mac', 18)->nullable()->comment('MAC地址');
             $table->string('name', 50)->nullable()->comment('名称');
-            $table->string('machine_name', 50)->nullable()->comment('机器名');
+            $table->string('machine', 50)->nullable()->comment('机器名');
             $table->string('username', 50)->nullable()->comment('用户名');
             $table->string('password', 256)->nullable()->comment('密码');
             $table->text('memo')->nullable()->comment('备注');
